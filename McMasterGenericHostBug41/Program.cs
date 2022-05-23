b3328672c6341ef101ff78ede54d333600a8a274
Aliaksandr Dabratsou <aliaksandr.dabratso1@ihsmarkit.com>
2022-05-23 20:31:00 +0300

    Add project files.

---
+++ McMasterGenericHostBug41/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+await new HostBuilder()
+    .ConfigureLogging((context, builder) =>
+    {
+        builder.AddConsole(opt =>
+        {
+            opt.TimestampFormat = "[HH:mm:ss]";
+        });
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddSingleton<TestCommand>();
+    })
+    .RunCommandLineApplicationAsync<App>(args);
+
+
+[Subcommand(typeof(TestCommand))]
+public class App : CommandLineApplication
+{
+}
+
+[Command(Name = "test")]
+public class TestCommand
+{
+    public void OnExecute()
+    {
+        Console.WriteLine("hello world.");
+    }
+}
